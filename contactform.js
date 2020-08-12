aa3dcbd0d9bf08626974713a2a8ebe95cf5af8f6
ken.again <kpowerperth@gmail.com>
2020-08-12 11:09:10 +0800

    Customised Contact Form

--- contactform.js
+++ contactform.js
@@ -7,28 +7,26 @@ $(document).ready(function () {
         var email = $('.email').val()
         var subject = $('.subject').val()
         var message = $('.message').val()
+        var name = $('.name').val()
         var statusElm = $('.status')
         statusElm.empty()
 
         if(email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusElm.append('<div>Email is valid</div>')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Email is NOT valid</div>')
+            statusElm.append('<div class="alert alert-danger">Email is NOT valid</div>')
         }
         
-        if(subject.length >= 2) {
-            statusElm.append('<div>Email is valid</div>')
+        if(name.length >= 2) {
         } else {
             event.preventDefault()
-            statusElm.append('<div>Email is NOT valid</div>')
+            statusElm.append('<div class="alert alert-danger">Name is NOT valid</div>')
         }
 
         if(message.length >= 10) {
-            statusElm.append('<div>Message is valid</div>')
         } else {
             event.preventDefault()
-            statusElm.append('<div>Message is NOT valid</div>')
+            statusElm.append('<div class="alert alert-danger">Message is NOT valid</div>')
         }
     })
 })
